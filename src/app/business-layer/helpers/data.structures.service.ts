ac038a89673fcc8e31f549251df72f8f675b7e64
Will <will.streeter@gmail.com>
2017-10-23 00:02:53 -0700

    update all

--- src/app/business-layer/helpers/data.structures.service.ts
+++ src/app/business-layer/helpers/data.structures.service.ts
@@ -21,30 +21,36 @@ export class DataStructureServices {
         propertyData.forEach((element) => {
             let monthlyRent = element.financial  && element.financial.monthlyRent ? element.financial.monthlyRent : 0;
             let listPrice =  element.financial && element.financial.listPrice ? element.financial.listPrice: 0;
+            let address1 =   element.address.address1?  element.address.address1:'';
+            let city =   element.address.city?  element.address.city:'';
+            let county =   element.address.county?  element.address.county:'';
+            let country =   element.address.country?  element.address.country:'';
+            let district =   element.address.district?  element.address.district:'';
+            let state =   element.address.state?  element.address.state:'';
             let  propModel:PropertyModel = <PropertyModel>{}
             propModel.id= element.id,
                 // address.address1
-            propModel.address1= element.address.address1,
+            propModel.address1= address1,
                 // address.city
-            propModel.city= element.address.city,
+            propModel.city= city,
                 // address.county
-            propModel.county= element.address.county,
+            propModel.county= county,
                 // address.country
-            propModel.country= element.address.country,
+            propModel.country= country,
                 // address.district
-            propModel.district= element.address.district,
+            propModel.district= district,
                 // address.state
-            propModel.state= element.address.state,
+            propModel.state= state,
                 // address.zip
             propModel.zip= element.address.zip,
                 // address.zipPlus4
             propModel.zipPlus4= element.address.zipPlus4,
                 //combined
-            propModel.addressCombined =  element.address.address1+' '+element.address.city+' '+
-                                         element.address.county+' '+element.address.state+' '+
-                                         element.address.zip+' '+element.address.country,
+            propModel.addressCombined =  address1+' '+city+' '+
+                                         county+' '+state+' '+
+                                         element.address.zip+' '+country,
                 // physical.yearBuilt
-            propModel.yearBuilt = element.physical ? element.physical.yearBuilt : 'no date',
+            propModel.yearBuilt = element.physical ? element.physical.yearBuilt : '-',
                 // financial.listPrice
             propModel.listPrice=  listPrice,
                 // financial.monthlyRent
